5d558157b81c7638a3b8c9aff65b0d934c5d27d2
laynefa <layne.j.fabian@gmail.com>
2020-04-16 23:58:29 -0400

    Flesh out a=b logic

--- journal.js
+++ journal.js
@@ -9,16 +9,73 @@ function addToJournal(){
     value2 = document.getElementById('value2').value;
     
     if (!(letter === "") && !(value === "")){
-        if (letterOperation == "+" && !(letter2==="")){
-            intValue = parseInt(value, 10);
-            if (journal.has(letter)){
-                journal.set(letter2, (intValue - parseInt(journal.get(letter), 10)).toString());
+        if (letterOperation == '='){
+            if (journal.has(value) && journal.has(letter)){
+                console.log('both values cannot exist in journal');
             }
-            else if(journal.has(letter2)){
-                journal.set(letter, (intValue - parseInt(journal.get(letter2), 10)).toString());
+            else if(journal.has(value)){
+                if (isNaN(letter)){
+                    journal.set(letter, journal.get(value))
+                }
+                else{
+                    console.log('letter 1 must be a letter');
+                }
+            }   
+            else if (journal.has(letter)){
+                if(isNaN(value)){
+                    journal.set(value, journal.get(letter));
+                }
             }
             else{
-                console.log('Neither value exists')
+                if (isNaN(letter) && isNaN(value)){
+                    console.log('Either the letter or value must be a number');
+                }
+                else if (isNaN(letter)){
+                    journal.set(letter, value);
+                }
+                else if (isNaN(value)){
+                    journal.set(value, letter);
+                }
+                else{
+                    console.log('Both can not be numbers')
+                }
+            }
+        }
+        else if (letterOperation == "+" && !(letter2==="")){
+            if (isNaN(value)){
+                //if value is not a number, then check if that value is a key in the map.
+                //if it is, then one of letter 1 or letter 2 must be in the map
+                //if it is not, then letter 1 and letter 2 must be in the map
+                if (journal.has(value)){
+                    if (journal.has(letter) && journal.has(letter2)){
+                        alert('Both letters can not exist in the journal already')
+                    }
+                    else if (journal.has(letter)){
+                        if (isNaN(letter2)){
+                            journal.set(letter2, (parseInt(value, 10) - parseInt(journal.get(letter1), 10)).toString())
+                        }
+                        else{
+                            alert('letter 2 must be a letter since letter 1 exists in the journal')
+                        }
+                    }
+                    else{
+                        if (isNaN(letter1)){
+                            journal.set(letter1, (parseInt(value, 10) - parseInt(journal.get(letter2), 10)).toString())
+                        }
+                    }
+                }
+            }
+            else{
+                intValue = parseInt(value, 10);
+                if (journal.has(letter)){
+                    journal.set(letter2, (intValue - parseInt(journal.get(letter), 10)).toString());
+                }
+                else if(journal.has(letter2)){
+                    journal.set(letter, (intValue - parseInt(journal.get(letter2), 10)).toString());
+                }
+                else{
+                    alert('Neither value exists')
+                }
             }
         }
         else if(valueOperation == "+" && !(value2==="")){
@@ -26,12 +83,6 @@ function addToJournal(){
                 result = parseInt(journal.get(value), 10) + parseInt(journal.get(value2), 10)
                 journal.set(letter, result.toString())
             }
-        }
-        else if (journal.has(value)){
-            journal.set(letter, journal.get(value))
-        }
-        else if (!journal.has(letter)){
-            journal.set(letter, value);
         }
         console.log(journal);
     }
@@ -50,5 +101,14 @@ function hideElements(){
     }
     else{
         document.getElementById('letter2Label').style.display = 'block';
+    }
+}
+
+function hideOp2Elements(){
+    if (document.getElementById('operation2').value == '+'){
+        document.getElementById('value2Label').style.display = 'block';
+    }
+    else{
+        document.getElementById('value2Label').style.display = 'none';
     }
 }
